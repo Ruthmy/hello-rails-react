201ddc7bad06de94cef95db27b9cb87556c12888
Ruthmy <ruthmy17@gmail.com>
2023-10-24 15:44:01 -0600

    Modified 'greetings_controller.rb' -controller- - Added an api action

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+# This controller handles API requests for greetings.
 class GreetingsController < ApplicationController
   def api
+    random_greeting = Greeting.order('RANDOM()').first
+    render json: { message: random_greeting&.message || 'No greetings found' }
   end
 end
